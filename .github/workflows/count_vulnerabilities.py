313ec02d5165ec20e9bf5562c433d2bee789a49a
shreyag28 <76720325+shreyag28@users.noreply.github.com>
2023-12-04 22:28:54 +0530

    Create count_vulnerabilities.py

---
+++ .github/workflows/count_vulnerabilities.py
@@ -0,0 +1,11 @@
+import json
+import sys
+
+# Read the JSON data from stdin
+report_data = json.load(sys.stdin)
+
+# Count the number of vulnerabilities
+vulnerability_count = len(report_data["Vulnerabilities"])
+
+# Print the vulnerability count
+print(vulnerability_count)
