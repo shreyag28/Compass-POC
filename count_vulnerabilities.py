e9dde84d8a701b9c6201d6ae652fd52496a89121
shreyag28 <76720325+shreyag28@users.noreply.github.com>
2023-12-06 16:11:22 +0530

    Update count_vulnerabilities.py

--- count_vulnerabilities.py
+++ count_vulnerabilities.py
@@ -6,11 +6,21 @@ import sys
 report_data = json.load(sys.stdin)
 
 # Print the entire JSON data for inspection
-print("Complete JSON Data:", report_data)
+#print("Complete JSON Data:", report_data)
 
 # Count the number of vulnerabilities
 # vulnerability_count = len(report_data.get("VulnerabilityID", []))
-vulnerability_count = len(report_data['VulnerabilityID'])
+# vulnerability_count = len(report_data['VulnerabilityID'])
 
-# Print the vulnerability count
-print("Vulnerability count:", vulnerability_count)
+if 'VulnerabilityID' in report_data:
+    # Extract the list of vulnerabilities
+    vulnerabilities = report_data['VulnerabilityID']
+
+    # Count the occurrences of 'VulnerabilityID' in the list
+    vulnerability_count = sum(1 for vulnerability in vulnerabilities if 'VulnerabilityID' in vulnerability)
+    # Print the vulnerability count
+    print("Vulnerability count:", vulnerability_count)
+
+
+
+
